a5a303fbb70bd1eb3983ed85a304fbe92c7da1fc
John Doe <johndoe@email.com>
2024-10-10 19:17:23 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<!-- vbase-3-ts-se -->\
+
+<script setup lang="ts">
+import { reactive, ref } from 'vue';
+
+
+//สร้าง ref สำหรับเก็บข้อมูล count
+const count = ref(0)
+const myuser = ref({
+  name: 'Natthpat Phummek',
+  age: 22
+})
+
+// เก็บ array
+const items = ref([1, 2, 3, 4, 5])
+
+// สร้าง reactive สำหรับเก็บข้อมูล user
+const user = reactive({
+  name: 'Fes F1',
+  age: 21
+})
+
+
+</script>
+
+<template>
+  
+  <button @click="count++">+ Increment</button>
+  <h1>Counter: {{ count }}</h1>
+  <button @click="count--">- Decrement</button>
+
+  <h1>{{ user.name }} {{ user.age }}</h1>
+  <h1>{{ myuser.name }} {{ myuser.age }}</h1>
+
+  <ul>
+    <li v-for="items in items" :key="items">{{ items }}</li>
+  </ul>
+
+</template>
+
+<style scoped>
+
+</style>
